060145cbf43a65140dcfbe5a1b947fa5315f4426
Ali Hasani <a.hassssani@gmail.com>
2020-06-23 22:44:28 +0430

    (chore) remove instance.ts (#4)

--- mod.ts
+++ mod.ts
@@ -1,5 +1,6 @@
-class Coffe {
+class Coffee {
   private config: object = {};
+  private currentLocation: string = "";
   async load(filename?: string | URL) {
     let configContent;
     if (filename) {
@@ -10,20 +11,53 @@ class Coffe {
     this.config = this.parse(configContent);
   }
 
-  async getDefaultConfig() {
+  private async getDefaultConfig() {
     let defaultConfig = await Deno.readTextFile("./config/config.json");
     return defaultConfig;
   }
 
-  parse(configContent: string) {
+  private parse(configContent: string) {
     return JSON.parse(configContent);
   }
 
   get(location: string) {
-    return location.split(".").reduce((previuse: any, current) => {
+    this.currentLocation = location;
+    return this;
+  }
+
+  toNumber(): number {
+    let configItem = this.getConfigItem();
+    if (typeof configItem !== "number") {
+      throw new Error(`${this.currentLocation} is not a number`);
+    }
+    return configItem;
+  }
+
+  toString(): string {
+    let configItem = this.getConfigItem();
+    if (typeof configItem !== "string") {
+      throw new Error(`${this.currentLocation} is not a string`);
+    }
+    return configItem;
+  }
+
+  toBoolean(): boolean {
+    let configItem = this.getConfigItem();
+    if (typeof configItem !== "boolean") {
+      throw new Error(`${this.currentLocation} is not a string`);
+    }
+    return configItem;
+  }
+
+  toAny(): any {
+    return this.getConfigItem();
+  }
+
+  private getConfigItem() {
+    return this.currentLocation.split(".").reduce((previuse: any, current) => {
       return previuse[current];
     }, this.config);
   }
 }
 
-export default new Coffe();
+export default new Coffee();
