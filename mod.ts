efe8ed24a47aff194304cc9800f03ccc8fce8011
M. Bagher Abiat <zorofight94@gmail.com>
2020-06-28 19:16:49 +0430

    yml and configFile support

--- mod.ts
+++ mod.ts
@@ -1,174 +1,183 @@
-import { parse as ymlParse, JSON_SCHEMA } from 'https://deno.land/std/encoding/yaml.ts';
-import Validateable from './src/Validateable.ts';
-import lensProp from './src/lensProp.ts';
-import deepExtend from './src/deepExtend.ts';
-import deepObjectMap from './src/deepObjectMap.ts';
+import {
+  parse as ymlParse,
+  JSON_SCHEMA,
+} from "https://deno.land/std/encoding/yaml.ts";
+import Validateable from "./src/Validateable.ts";
+import lensProp from "./src/lensProp.ts";
+import deepExtend from "./src/deepExtend.ts";
+import deepObjectMap from "./src/deepObjectMap.ts";
 
 export interface Configs {
-	[key: string]: Configs | string | number | boolean;
+  [key: string]: Configs | string | number | boolean;
 }
 
 interface Parser {
-	(raw: string): Configs;
+  (raw: string): Configs;
 }
 
 type LoadOptions = {
-	configDir: string;
-	customEnvVarFileName: string;
-	configFile?: string;
-	env?: string;
+  configDir: string;
+  customEnvVarFileName: string;
+  configFile?: string;
+  env?: string;
 };
 
 interface RuntimeAPI {
-	envName: string;
-	getEnvVar(key: string): string | undefined;
-	getRuntimeEnv(): string | undefined;
-	readFileIfExist(path: string): string | undefined;
-	isDirExist(path: string): boolean;
+  envName: string;
+  getEnvVar(key: string): string | undefined;
+  getRuntimeEnv(): string | undefined;
+  readFileIfExist(path: string): string | undefined;
+  isDirExist(path: string): boolean;
 }
 
 class DenoAPI implements RuntimeAPI {
-	envName = 'DENO_ENV';
-	getEnvVar(key: string) {
-		return Deno.env.get(key);
-	}
+  envName = "DENO_ENV";
+  getEnvVar(key: string) {
+    return Deno.env.get(key);
+  }
 
-	readFileIfExist(path: string) {
-		try {
-			return Deno.readTextFileSync(path);
-		} catch (e) {
-			if (e instanceof Deno.errors.NotFound) return;
-			throw e;
-		}
-	}
-	getRuntimeEnv() {
-		return Deno.env.get('DENO_ENV');
-	}
+  readFileIfExist(path: string) {
+    try {
+      return Deno.readTextFileSync(path);
+    } catch (e) {
+      if (e instanceof Deno.errors.NotFound) return;
+      throw e;
+    }
+  }
+  getRuntimeEnv() {
+    return Deno.env.get("DENO_ENV");
+  }
 
-	isDirExist(path: string) {
-		try {
-			Deno.readDirSync(path);
-			return true;
-		} catch (e) {
-			if (e instanceof Deno.errors.NotFound) return false;
-			throw e;
-		}
-	}
+  isDirExist(path: string) {
+    try {
+      Deno.readDirSync(path);
+      return true;
+    } catch (e) {
+      if (e instanceof Deno.errors.NotFound) return false;
+      throw e;
+    }
+  }
 }
 
 const defaultOptions: LoadOptions = {
-	configDir: './config',
-	customEnvVarFileName: 'custom-environment-variables'
+  configDir: "./config",
+  customEnvVarFileName: "custom-environment-variables",
 };
 
 export class Coffee {
-	private loadOptions: LoadOptions = defaultOptions;
-	private isLoaded = false;
-	private fileExts = [
-		'json',
-		'yml'
-	];
+  private loadOptions: LoadOptions = defaultOptions;
+  private isLoaded = false;
+  private fileExts = [
+    "json",
+    "yml",
+  ];
 
-	runtimeAPI: RuntimeAPI = new DenoAPI();
-	parsers: { [k: string]: Parser } = {
-		json: (t: string) => JSON.parse(t),
-		yml: (t: string) => this.ymlReader(t)
-	};
-	configs: Configs = {};
-	private ymlReader(t: string): Configs {
-		// ymlParse returns unknown, We should convert its type to Configs type
-		const parsedYml = ymlParse(t, {
-			schema: JSON_SCHEMA,
-			json: true
-		});
-		const ymlToJSON = JSON.stringify(parsedYml);
+  runtimeAPI: RuntimeAPI = new DenoAPI();
+  parsers: { [k: string]: Parser } = {
+    json: (t: string) => JSON.parse(t),
+    yml: (t: string) => this.ymlReader(t),
+  };
+  configs: Configs = {};
+  private ymlReader(t: string): Configs {
+    // ymlParse returns unknown, We should convert its type to Configs type
+    const parsedYml = ymlParse(t, {
+      schema: JSON_SCHEMA,
+      json: true,
+    });
+    const ymlToJSON = JSON.stringify(parsedYml);
 
-		return JSON.parse(ymlToJSON);
-	}
+    return JSON.parse(ymlToJSON);
+  }
 
-	private readConfigFile(fileName: string): Configs | undefined {
-		const rawConfigs = this.runtimeAPI.readFileIfExist(this.loadOptions.configDir + '/' + fileName);
+  private readConfigFile(fileName: string): Configs | undefined {
+    const rawConfigs = this.runtimeAPI.readFileIfExist(
+      this.loadOptions.configDir + "/" + fileName,
+    );
 
-		if (!rawConfigs) return undefined;
-		const fileExt = fileName.split('.').slice(-1)[0];
-		if (this.parsers[fileExt]) return this.parsers[fileExt](rawConfigs);
+    if (!rawConfigs) return undefined;
+    const fileExt = fileName.split(".").slice(-1)[0];
+    if (this.parsers[fileExt]) return this.parsers[fileExt](rawConfigs);
 
-		throw new Error(`"${fileExt}" file extension not supported!`);
-	}
+    throw new Error(`"${fileExt}" file extension not supported!`);
+  }
 
-	private loadConfigs(fileName = 'default') {
-		for (const fileExt of this.fileExts) {
-			let configs = this.readConfigFile(`${fileName}.${fileExt}`);
-			if (configs) {
-				deepExtend(this.configs, configs);
-				break;
-			}
-		}
-	}
+  private loadConfigs(fileName = "default") {
+    for (const fileExt of this.fileExts) {
+      let configs = this.readConfigFile(`${fileName}.${fileExt}`);
+      if (configs) {
+        deepExtend(this.configs, configs);
+        break;
+      }
+    }
+  }
 
-	private loadEnvRelativeConfigs() {
-		const runtimeENV = this.runtimeAPI.getRuntimeEnv();
-		if (runtimeENV) {
-			for (const fileExt of this.fileExts) {
-				const envConfigs = this.readConfigFile(`${runtimeENV}.${fileExt}`);
-				if (envConfigs) {
-					deepExtend(this.configs, envConfigs);
-					break;
-				}
-			}
-		}
-	}
+  private loadEnvRelativeConfigs() {
+    const runtimeENV = this.runtimeAPI.getRuntimeEnv();
+    if (runtimeENV) {
+      for (const fileExt of this.fileExts) {
+        const envConfigs = this.readConfigFile(`${runtimeENV}.${fileExt}`);
+        if (envConfigs) {
+          deepExtend(this.configs, envConfigs);
+          break;
+        }
+      }
+    }
+  }
 
-	private loadCustomEnvVarConfigs() {
-		for (const fileExt of this.fileExts) {
-			const customEnvVars = this.readConfigFile(`${this.loadOptions.customEnvVarFileName}.${fileExt}`);
+  private loadCustomEnvVarConfigs() {
+    for (const fileExt of this.fileExts) {
+      const customEnvVars = this.readConfigFile(
+        `${this.loadOptions.customEnvVarFileName}.${fileExt}`,
+      );
 
-			if (customEnvVars) {
-				deepObjectMap((value) => {
-					if (typeof value !== 'string') return;
-					return this.runtimeAPI.getEnvVar(value);
-				}, customEnvVars);
+      if (customEnvVars) {
+        deepObjectMap((value) => {
+          if (typeof value !== "string") return;
+          return this.runtimeAPI.getEnvVar(value);
+        }, customEnvVars);
 
-				deepExtend(this.configs, customEnvVars);
-				break;
-			}
-		}
-	}
+        deepExtend(this.configs, customEnvVars);
+        break;
+      }
+    }
+  }
 
-	load(opts: Partial<LoadOptions> = {}): void {
-		this.loadOptions = deepExtend(defaultOptions, opts);
-		const result = this.runtimeAPI.isDirExist(this.loadOptions.configDir);
-		if (!result) {
-			throw new Error(`configDir: ${this.loadOptions.configDir} is not existed!`);
-		}
+  load(opts: Partial<LoadOptions> = {}): void {
+    this.loadOptions = deepExtend(defaultOptions, opts);
+    const result = this.runtimeAPI.isDirExist(this.loadOptions.configDir);
+    if (!result) {
+      throw new Error(
+        `configDir: ${this.loadOptions.configDir} is not existed!`,
+      );
+    }
 
-		this.loadConfigs(this.loadOptions.configFile);
-		this.loadEnvRelativeConfigs();
-		this.loadCustomEnvVarConfigs();
+    this.loadConfigs(this.loadOptions.configFile);
+    this.loadEnvRelativeConfigs();
+    this.loadCustomEnvVarConfigs();
 
-		this.isLoaded = true;
-	}
+    this.isLoaded = true;
+  }
 
-	get(path: string): Validateable {
-		if (this.isLoaded === false) this.load();
-		const v = lensProp(this.configs, path);
-		return new Validateable(v);
-	}
+  get(path: string): Validateable {
+    if (this.isLoaded === false) this.load();
+    const v = lensProp(this.configs, path);
+    return new Validateable(v);
+  }
 
-	has(path: string): boolean {
-		if (this.isLoaded === false) this.load();
-		try {
-			const v = lensProp(this.configs, path);
-			return v !== undefined;
-		} catch (e) {
-			return false;
-		}
-	}
+  has(path: string): boolean {
+    if (this.isLoaded === false) this.load();
+    try {
+      const v = lensProp(this.configs, path);
+      return v !== undefined;
+    } catch (e) {
+      return false;
+    }
+  }
 
-	set(path: string, value: string | number | boolean): void {
-		if (this.isLoaded === false) this.load();
-		lensProp(this.configs, path, value);
-	}
+  set(path: string, value: string | number | boolean): void {
+    if (this.isLoaded === false) this.load();
+    lensProp(this.configs, path, value);
+  }
 }
 
 export default new Coffee();
--- test/unit/Coffee.spec.ts
+++ test/unit/Coffee.spec.ts
@@ -1,5 +1,5 @@
-import { Coffee, Configs } from '../../mod.ts';
-import { assertEquals } from 'https://deno.land/std@0.58.0/testing/asserts.ts';
+import { Coffee, Configs } from "../../mod.ts";
+import { assertEquals } from "https://deno.land/std@0.58.0/testing/asserts.ts";
 
 const coffee = new Coffee();
 
@@ -7,126 +7,126 @@ coffee.runtimeAPI.getEnvVar = () => undefined;
 coffee.runtimeAPI.readFileIfExist = () => undefined;
 coffee.runtimeAPI.getRuntimeEnv = () => undefined;
 
-Deno.test('it should read default.json file configs', () => {
-	coffee.runtimeAPI.readFileIfExist = function(path) {
-		if (path.includes('default.json')) {
-			return JSON.stringify({
-				a: {
-					a: true,
-					b: 3,
-					t: 'default'
-				}
-			});
-		}
-		return undefined;
-	};
+Deno.test("it should read default.json file configs", () => {
+  coffee.runtimeAPI.readFileIfExist = function (path) {
+    if (path.includes("default.json")) {
+      return JSON.stringify({
+        a: {
+          a: true,
+          b: 3,
+          t: "default",
+        },
+      });
+    }
+    return undefined;
+  };
 
-	coffee.load();
+  coffee.load();
 
-	const b: number = coffee.get('a.b').number();
-	const t: string = coffee.get('a.t').string();
-	const a: boolean = coffee.get('a.a').boolean();
+  const b: number = coffee.get("a.b").number();
+  const t: string = coffee.get("a.t").string();
+  const a: boolean = coffee.get("a.a").boolean();
 
-	assertEquals(b, 3);
-	assertEquals(t, 'default');
-	assertEquals(a, true);
+  assertEquals(b, 3);
+  assertEquals(t, "default");
+  assertEquals(a, true);
 });
 
-Deno.test('coffee.has', () => {
-	assertEquals(coffee.has('a.b'), true);
-	assertEquals(coffee.has('a'), true);
-	assertEquals(coffee.has('a.c'), false);
+Deno.test("coffee.has", () => {
+  assertEquals(coffee.has("a.b"), true);
+  assertEquals(coffee.has("a"), true);
+  assertEquals(coffee.has("a.c"), false);
 });
 
-Deno.test('coffee.set -> it should create new config', () => {
-	coffee.set('b.c', 'newValue');
-	assertEquals((coffee.configs.b as Configs).c, 'newValue');
-	assertEquals(coffee.get('b.c').string(), 'newValue');
+Deno.test("coffee.set -> it should create new config", () => {
+  coffee.set("b.c", "newValue");
+  assertEquals((coffee.configs.b as Configs).c, "newValue");
+  assertEquals(coffee.get("b.c").string(), "newValue");
 });
 
-Deno.test('coffee.set -> it should override existing config', () => {
-	coffee.set('b.c', 'newValue2');
-	assertEquals((coffee.configs.b as Configs).c, 'newValue2');
-	assertEquals(coffee.get('b.c').string(), 'newValue2');
+Deno.test("coffee.set -> it should override existing config", () => {
+  coffee.set("b.c", "newValue2");
+  assertEquals((coffee.configs.b as Configs).c, "newValue2");
+  assertEquals(coffee.get("b.c").string(), "newValue2");
 });
 
-Deno.test('It should load [DENO_ENV].json file configs', () => {
-	coffee.runtimeAPI.getRuntimeEnv = () => 'development';
+Deno.test("It should load [DENO_ENV].json file configs", () => {
+  coffee.runtimeAPI.getRuntimeEnv = () => "development";
 
-	coffee.runtimeAPI.readFileIfExist = function(path) {
-		if (path.includes('default.json')) {
-			return JSON.stringify({
-				a: {
-					a: true,
-					b: 3,
-					t: 'default'
-				}
-			});
-		}
+  coffee.runtimeAPI.readFileIfExist = function (path) {
+    if (path.includes("default.json")) {
+      return JSON.stringify({
+        a: {
+          a: true,
+          b: 3,
+          t: "default",
+        },
+      });
+    }
 
-		if (path.includes('development.json')) {
-			return JSON.stringify({
-				a: {
-					t: 'dev'
-				}
-			});
-		}
+    if (path.includes("development.json")) {
+      return JSON.stringify({
+        a: {
+          t: "dev",
+        },
+      });
+    }
 
-		return undefined;
-	};
+    return undefined;
+  };
 
-	coffee.load();
+  coffee.load();
 
-	const b: number = coffee.get('a.b').number();
-	const t: string = coffee.get('a.t').string();
-	const a: boolean = coffee.get('a.a').boolean();
+  const b: number = coffee.get("a.b").number();
+  const t: string = coffee.get("a.t").string();
+  const a: boolean = coffee.get("a.a").boolean();
 
-	assertEquals(b, 3);
-	assertEquals(t, 'dev');
-	assertEquals(a, true);
+  assertEquals(b, 3);
+  assertEquals(t, "dev");
+  assertEquals(a, true);
 });
 
-Deno.test('coffee.has', () => {
-	assertEquals(coffee.has('a.b'), true);
-	assertEquals(coffee.has('a'), true);
-	assertEquals(coffee.has('a.c'), false);
+Deno.test("coffee.has", () => {
+  assertEquals(coffee.has("a.b"), true);
+  assertEquals(coffee.has("a"), true);
+  assertEquals(coffee.has("a.c"), false);
 });
 
-Deno.test('It should load custom-environment-variable file configs', () => {
-	coffee.runtimeAPI.getRuntimeEnv = function() {
-		return 'development';
-	};
+Deno.test("It should load custom-environment-variable file configs", () => {
+  coffee.runtimeAPI.getRuntimeEnv = function () {
+    return "development";
+  };
 
-	coffee.runtimeAPI.readFileIfExist = function(path) {
-		if (path.includes('custom-environment-variables.json')) {
-			return JSON.stringify({
-				a: {
-					port: 'PORT',
-					secret: 'SECRET'
-				}
-			});
-		}
+  coffee.runtimeAPI.readFileIfExist = function (path) {
+    if (path.includes("custom-environment-variables.json")) {
+      return JSON.stringify({
+        a: {
+          port: "PORT",
+          secret: "SECRET",
+        },
+      });
+    }
 
-		return undefined;
-	};
+    return undefined;
+  };
 
-	coffee.runtimeAPI.getEnvVar = function(key: string) {
-		if (key === 'PORT') return '3000';
-		if (key === 'SECRET') return 'ABC';
-		return;
-	};
+  coffee.runtimeAPI.getEnvVar = function (key: string) {
+    if (key === "PORT") return "3000";
+    if (key === "SECRET") return "ABC";
+    return;
+  };
 
-	coffee.load();
+  coffee.load();
 
-	const port: string = coffee.get('a.port').string();
-	const secret: string = coffee.get('a.secret').string();
+  const port: string = coffee.get("a.port").string();
+  const secret: string = coffee.get("a.secret").string();
 
-	assertEquals(port, '3000');
-	assertEquals(secret, 'ABC');
+  assertEquals(port, "3000");
+  assertEquals(secret, "ABC");
 });
 
-Deno.test('coffee.has', () => {
-	assertEquals(coffee.has('a.b'), true);
-	assertEquals(coffee.has('a'), true);
-	assertEquals(coffee.has('a.c'), false);
+Deno.test("coffee.has", () => {
+  assertEquals(coffee.has("a.b"), true);
+  assertEquals(coffee.has("a"), true);
+  assertEquals(coffee.has("a.c"), false);
 });
