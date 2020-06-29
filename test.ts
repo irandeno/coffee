499dac0e8ca70a68799e018e9f5d5a68c885aa3c
M. Bagher Abiat <zorofight94@gmail.com>
2020-06-29 16:58:07 +0430

    test bug for ymlReader solved

--- test.ts
+++ test.ts
@@ -1,7 +1,7 @@
-import "./test/unit/Coffee.spec.ts";
-import "./test/unit/lensProp.spec.ts";
-import "./test/unit/Validateable.spec.ts";
-import "./test/unit/deepExtend.spec.ts";
-import "./test/unit/deepObjectMap.spec.ts";
-
-import "./test/integration/mod.test.ts";
+import './test/unit/Coffee.spec.ts';
+import './test/unit/lensProp.spec.ts';
+import './test/unit/Validateable.spec.ts';
+import './test/unit/deepExtend.spec.ts';
+import './test/unit/deepObjectMap.spec.ts';
+import './test/unit/ymlReader.spec.ts';
+import './test/integration/mod.test.ts';
--- test/unit/ymlReader.spec.ts
+++ test/unit/ymlReader.spec.ts
@@ -1,11 +1,10 @@
-import { assertEquals } from "https://deno.land/std@0.58.0/testing/asserts.ts";
-import { ymlReader } from "../../src/ymlReader.ts";
+import { assertEquals } from 'https://deno.land/std@0.58.0/testing/asserts.ts';
+import { ymlReader } from '../../src/ymlReader.ts';
 
-Deno.test("ymlReader test -> return 4", () => {
-  const parsedYml = ymlReader(`---
-    a:
+Deno.test('ymlReader test -> return 4', () => {
+	const parsedYml = ymlReader(`---
       b: 4
     `);
-  const b = parsedYml.a.b;
-  assertEquals(b, 4);
+	const b = parsedYml.b;
+	assertEquals(b, 4);
 });
